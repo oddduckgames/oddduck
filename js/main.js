eeaabc48465c806b21b122d58460b456e21635cf
Leonel Ramos <lramos.terp@gmail.com>
2020-04-26 10:25:30 -0400

    moved global nav bar into js file

--- js/main.js
+++ js/main.js
@@ -1,25 +1,87 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     // Get all "navbar-burger" elements
+    const parser = new DOMParser();
+    const main_nav = parser.parseFromString(
+        `<nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+        <div class="container">
+  
+          <div class="navbar-brand">
+            <a href="../oddduckgames.github.io/index.html" class="navbar-item is-family-secondary is-size-4 is-hidden-desktop">oddduck games</a>
+            <a href="../oddduckgames.github.io/index.html" class="navbar-item is-family-secondary is-size-3 is-hidden-mobile is-hidden-tablet-only">oddduck games</a>
+            <div class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="menu-options">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </div>
+          </div>
+  
+          <div id="menu-options" class="navbar-menu">
+            <div class="navbar-start">
+              <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                  games
+                </a>
+                <div class="navbar-dropdown">
+                  <a href="../oddduckgames.github.io/pages/the-line.html" class="navbar-item">The Line</a>
+                  <hr class="navbar-divider">
+                  <a href="../oddduckgames.github.io/pages/ina.html" class="navbar-item">Tales of the Basilisk: Ina</a>
+                </div>
+              </div>
+              <a href="../oddduckgames.github.io/pages/about.html" class="navbar-item">about</a>
+              <a href="../oddduckgames.github.io/pages/forums.html" class="navbar-item">forums</a>
+              <a href="../oddduckgames.github.io/pages/blog.html" class="navbar-item">dev-blog</a>
+              <a href="../oddduckgames.github.io/pages/contact.html" class="navbar-item">contact</a>
+            </div>
+            <div class="navbar-end">
+              <a href="https://www.youtube.com/channel/UCqzFJQ7_h8XI3jNtAsrEq9w/" class="navbar-item"><i class="fab fa-youtube"></i>
+                <span class="is-hidden-desktop">
+                  <strong>The official YouTube channel</strong>
+                  <br>
+                  <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tutorials, gameplay and more</em>
+                </span>
+              </a>
+              <a href="https://twitter.com/oddduckgames" class="navbar-item"><i class="fab fa-twitter"></i>
+                <span class="is-hidden-desktop">
+                  <strong>The official Twitter</strong>
+                  <br>
+                  <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stay updated or @ us</em>
+                </span>
+              </a>
+              <a href="https://www.twitch.tv/oddduckgames" class="navbar-item"><i class="fab fa-twitch"></i>
+                <span class="is-hidden-desktop">
+                  <strong>The official Twitch stream</strong>
+                  <br>
+                  <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Live announcments and tutorials</em>
+                </span>
+              </a>
+              <a href="https://github.com/oddduckgames" class="navbar-item"><i class="fab fa-github"></i>
+                <span class="is-hidden-desktop">
+                  <strong>The official GitHub</strong>
+                  <br>
+                  <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We love open source</em>
+                </span>
+              </a>
+            </div>
+  
+          </div>
+  
+        </div>
+      </nav>`, "text/html").getElementsByTagName("nav")[0]
+    
+    const body_container = document.getElementsByTagName('body')[0];
+    if(body_container.firstChild != null) body_container.insertBefore(main_nav, body_container.firstChild)
+    else body_container.appendChild(main_nav)
+    
     const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
     if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
       $navbarBurgers.forEach( el => {
         el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
           const target = el.dataset.target;
           const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
           el.classList.toggle('is-active');
           $target.classList.toggle('is-active');
-  
         });
       });
     }
-  
   });
