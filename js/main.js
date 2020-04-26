5dded50857572c456f42cc79612acb8824589971
Leonel Ramos <lramos.terp@gmail.com>
2020-04-26 15:25:19 -0400

    added dark mode

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    // Get all "navbar-burger" elements
     const parser = new DOMParser();
     const main_nav = parser.parseFromString(
-        `<nav class="navbar has-shadow is-spaced" role="navigation" aria-label="main navigation">
+        `<nav class="navbar is-spaced is-dark toggle-dark" role="navigation" aria-label="main navigation">
         <div class="container">
   
           <div class="navbar-brand">
@@ -13,59 +11,69 @@ document.addEventListener('DOMContentLoaded', () => {
             <a href="../oddduckgames.github.io/index.html" class="navbar-item is-family-secondary is-size-2 is-hidden-mobile is-hidden-tablet-only">
               <img src="../oddduckgames.github.io/img/duck.png">
               <strong>oddduck games</strong></a>
+            <span class="navbar-item">
+            <div class="field dark-switch is-hidden-desktop">
+              <input id="dark-mode-switch" type="checkbox" name="dark-mode-switch" class="switch dark-mode-switch is-rtl is-small" checked="checked">
+              <label for="dark-mode-switch"><i class="fas fa-lightbulb"></i></label>
+            </div>
+            </span>
             <div class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="menu-options">
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
             </div>
           </div>
-  
-          <div id="menu-options" class="navbar-menu">
+          
+          <div id="menu-options" class="navbar-menu toggle-dark">
             <div class="navbar-start">
               <div class="navbar-item has-dropdown is-hoverable">
-                <a class="navbar-link">
+                <a class="navbar-link toggle-dark">
                   games
                 </a>
-                <div class="navbar-dropdown">
-                  <a href="../oddduckgames.github.io/pages/the-line.html" class="navbar-item">The Line</a>
+                <div class="navbar-dropdown toggle-dark">
+                  <a href="../oddduckgames.github.io/pages/the-line.html" class="navbar-item toggle-dark">The Line</a>
                   <hr class="navbar-divider">
-                  <a href="../oddduckgames.github.io/pages/ina.html" class="navbar-item">Tales of the Basilisk: Ina</a>
+                  <a href="../oddduckgames.github.io/pages/ina.html" class="navbar-item toggle-dark">Tales of the Basilisk: Ina</a>
                 </div>
               </div>
-              <a href="../oddduckgames.github.io/pages/about.html" class="navbar-item">about</a>
-              <a href="../oddduckgames.github.io/pages/forums.html" class="navbar-item">forums</a>
-              <a href="../oddduckgames.github.io/pages/blog.html" class="navbar-item">dev-blog</a>
-              <a href="../oddduckgames.github.io/pages/contact.html" class="navbar-item">contact</a>
+              <a href="../oddduckgames.github.io/pages/about.html" class="navbar-item toggle-dark">about</a>
+              <a href="../oddduckgames.github.io/pages/forums.html" class="navbar-item toggle-dark">forums</a>
+              <a href="../oddduckgames.github.io/pages/blog.html" class="navbar-item toggle-dark">dev-blog</a>
+              <a href="../oddduckgames.github.io/pages/contact.html" class="navbar-item toggle-dark">contact</a>
             </div>
             <div class="navbar-end">
               <a href="https://www.youtube.com/channel/UCqzFJQ7_h8XI3jNtAsrEq9w/" class="navbar-item"><span style="color: #c4302b;"><i class="fab fa-youtube"></i></span>
-                <span class="is-hidden-desktop">
+                <span class="is-hidden-desktop toggle-dark">
                   <strong>The official YouTube channel</strong>
                   <br>
                   <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Tutorials, gameplay and more</em>
                 </span>
               </a>
               <a href="https://twitter.com/oddduckgames" class="navbar-item"><span style="color: #00acee;"><i class="fab fa-twitter"></i></span>
-                <span class="is-hidden-desktop">
+                <span class="is-hidden-desktop toggle-dark">
                   <strong>The official Twitter</strong>
                   <br>
                   <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stay updated or @ us</em>
                 </span>
               </a>
               <a href="https://www.twitch.tv/oddduckgames" class="navbar-item"><span style="color: #6441a5 ;"><i class="fab fa-twitch"></i></span>
-                <span class="is-hidden-desktop">
+                <span class="is-hidden-desktop toggle-dark">
                   <strong>The official Twitch stream</strong>
                   <br>
                   <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Live announcments and tutorials</em>
                 </span>
               </a>
               <a href="https://github.com/oddduckgames" class="navbar-item"><i class="fab fa-github"></i>
-                <span class="is-hidden-desktop">
+                <span class="is-hidden-desktop toggle-dark">
                   <strong>The official GitHub</strong>
                   <br>
                   <em>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We love open source</em>
                 </span>
               </a>
+              <div class="field dark-switch is-hidden-mobile is-hidden-tablet-only">
+                <input id="dark-mode-switch-desktop" type="checkbox" name="dark-mode-switch" class="switch dark-mode-switch is-rtl is-small" checked="checked">
+                <label for="dark-mode-switch-desktop"><i class="fas fa-lightbulb"></i></label>
+              </div>
             </div>
   
           </div>
@@ -86,6 +94,41 @@ document.addEventListener('DOMContentLoaded', () => {
           el.classList.toggle('is-active');
           $target.classList.toggle('is-active');
         });
+      });
+    }
+
+    document.getElementsByTagName("html")[0].classList.add("toggle-dark");
+    const dark_toggles = document.getElementsByClassName("toggle-dark");
+    for(let el of dark_toggles)
+    {
+      el.classList.add("has-background-dark");
+      el.classList.add("has-text-white");
+      el.classList.add("is-dark");
+    }
+    const dark_switches = document.getElementsByClassName("dark-mode-switch");
+    console.log(dark_switches)
+    for (let el of dark_switches) {
+      el.addEventListener('click', (event) => {
+        if(el.getAttribute("checked") === "checked")
+        {
+          el.setAttribute("checked", "unchecked");
+          for(let toggle of dark_toggles)
+          {
+            toggle.classList.remove("has-background-dark");
+            toggle.classList.remove("has-text-white");
+            toggle.classList.remove("is-dark");
+          }  
+        }
+        else
+        {
+          el.setAttribute("checked", "checked");
+          for(let toggle of dark_toggles)
+          {
+            toggle.classList.add("has-background-dark");
+            toggle.classList.add("has-text-white");
+            toggle.classList.add("is-dark");
+          } 
+        }   
       });
     }
   });
